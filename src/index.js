b31322b2c2cd9fff6d6a7b194fb0ffe5d19bbc19
apusapati574 <anudeeppusapati2016@gmail.com>
2024-10-14 19:48:00 +0530

    accessing component & props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* eslint-disable no-undef */
+const Notifications = (props) => {
+  const {className, notificationText, image} = props;
+  return (
+    <li className={`${className}`}>
+      <img src={`${image}`} alt="image" />
+      <p>{notificationText}</p>
+    </li>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Notifications</h1>
+    <ul>
+      <Notifications 
+        className="info-msg" 
+        image= "https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+        notificationText="Information Message" />
+      <Notifications className="success-msg"
+        image = "https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+        notificationText="Success Message" />
+      <Notifications className="warning-msg" 
+      image = "https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+      notificationText="Warning Message" />
+      <Notifications className="error-msg"
+      image = "https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+      notificationText="Error Message" />
+    </ul>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, document.getElementById("root"));
